bf94a057ee29b3471dcbff3bbfe487ba80dfcdac
Mote1337 <motevonsen@gmail.com>
2020-05-22 23:57:43 +0200

    fix parse_input

--- logparse/controller.rb
+++ logparse/controller.rb
@@ -23,6 +23,27 @@ class LogParserController
         parse_input user_input
       end
     end
+
+    def parse_input user_input
+      case user_input
+      when "\n"
+        #change controller likely!
+        #check the View's current interaction
+        #index to see what's next!
+      when "\e[A"
+        #up button ... update the view with an
+        #up action!
+      when "\e[B"
+        #down!
+      when "\e[C"
+        #right
+      when "\e[D"
+        #left
+      else
+        #send other input to a selected input field
+      end
+    end
+
   end
 
 end
